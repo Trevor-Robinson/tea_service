277ac637ef25c156f14bfb5c25d42d820b6eef69
Trevor Robinson <trevor.robi1254@gmail.com>
2021-06-08 11:12:22 -0600

    Create and test customer model and migration

---
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+describe 'customers API' do
+  it "returns a customer by id" do
+    id = create(:customer).id
+
+    get "/api/v1/customers/#{id}"
+
+    expect(response).to be_successful
+    customer = JSON.parse(response.body, symbolize_names: true)
+
+    expect(customer).to have_key(:id)
+    expect(customer[:id]).to be_an(Integer)
+    expect(customer).to have_key(:first_name)
+    expect(customer[:first_name]).to be_a(String)
+    expect(customer).to have_key(:last_name)
+    expect(customer[:last_name]).to be_a(String)
+    expect(customer).to have_key(:email)
+    expect(customer[:email]).to be_a(String)
+    expect(customer).to have_key(:address)
+    expect(customer[:address]).to be_a(String)
+  end
+end
